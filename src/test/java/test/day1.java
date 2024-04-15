a6376ea8d761c09a34114572bb6301c8c0d86d3e
expleoneer <david.hinteregger@expleogroup.com>
2024-04-15 10:33:30 +0200

    new tests added

--- src/test/java/test/day1.java
+++ src/test/java/test/day1.java
@@ -17,6 +17,21 @@ public class day1 {
         System.out.println("------------------------");
     }
 
+    @Test
+    public void testcase1(){
+        System.out.println("testcase1");
+    }
+
+    @Test
+    public void testcase2(){
+        System.out.println("testcase2");
+    }
+
+    @Test
+    public void testcase3(){
+        System.out.println("testcase3");
+    }
+
     @Parameters({"URL"})
     @Test
     public void SecondTest(String url){
